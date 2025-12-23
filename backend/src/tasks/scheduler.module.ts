9c77838fbcd322c479f4de1be73325ef3fd93250
Hwoarang91 <hwoara91@gmail.com>
2025-12-23 07:24:24 +0300

    fix: РґРѕР±Р°РІР»РµРЅ TelegramUserSession РІ TypeOrmModule.forFeature Рё РёРјРїРѕСЂС‚ TelegramModule РІ SchedulerModule

--- backend/src/tasks/scheduler.module.ts
+++ backend/src/tasks/scheduler.module.ts
@@ -6,16 +6,19 @@ import { Appointment } from '../entities/appointment.entity';
 import { Service } from '../entities/service.entity';
 import { Notification } from '../entities/notification.entity';
 import { User } from '../entities/user.entity';
+import { TelegramUserSession } from '../entities/telegram-user-session.entity';
 import { NotificationsModule } from '../modules/notifications/notifications.module';
 import { FinancialModule } from '../modules/financial/financial.module';
 import { SettingsModule } from '../modules/settings/settings.module';
+import { TelegramModule } from '../modules/telegram/telegram.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Appointment, Service, Notification, User]),
+    TypeOrmModule.forFeature([Appointment, Service, Notification, User, TelegramUserSession]),
     NotificationsModule,
     FinancialModule,
     SettingsModule,
+    TelegramModule, // Импортируем TelegramModule для доступа к TelegramUserSessionRepository
   ],
   controllers: [SchedulerController],
   providers: [SchedulerService],
